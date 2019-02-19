8030b51ab2b5e70fdc0e7a47317bd328183c5333
Manisha Pandey <37845282+Manisha53@users.noreply.github.com>
2019-02-20 00:24:38 +0530

    Add files via upload

---
+++ delete_ll.c
@@ -0,0 +1,122 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+struct node{
+    int data;
+    struct node* next;
+};
+
+ void printlist(struct node *n)
+ {
+        while(n!=NULL)
+            {
+            printf("%d\t\n",n->data);
+            n=n->next;
+            }
+    }
+void insert_beg(struct node** head_ref ,int val )
+{
+    struct node* beg=(struct node** )malloc(sizeof(struct node));   //node to be inserted in beginning
+    beg->data=val;
+    beg->next=*head_ref;
+    *head_ref=beg;
+}
+void deletenode(struct node ** head_ref,int key)      //deletion of node OF GIVEN VALUE from linked list
+{
+    struct node* temp=*head_ref,*prev;
+
+    if( temp!=NULL && temp->data==key ){
+        *head_ref=temp->next;
+        free(temp);
+        return;
+    }
+    while(temp!=NULL && temp->data!=key)
+    {
+        prev=temp;
+        temp=temp->next;
+    }
+    if(temp == NULL){
+        return;
+    }
+    prev->next=temp->next;
+        //temp->next=NULL;
+        free(temp);
+}
+
+void deletepos(struct node ** head_ref,int pos)
+{
+    int i;
+    struct node *temp=*head_ref,*prev;
+    if(*head_ref == NULL)
+    {
+        printf("linkedlist does not exists\n");
+    }
+    for(i=0;temp!=NULL && i<pos;i++)
+    {    prev=temp;
+        temp=temp->next;
+    }
+    if(temp==NULL)
+        return;
+    prev->next=temp->next;
+    free(temp);
+}
+
+void dellist(struct node** head_ref)
+{
+    struct node* temp=*head_ref;
+    struct node* nex;
+    while(temp!=NULL)
+        {
+            nex=temp->next;
+            free(temp);
+            temp=nex;
+        }
+
+        *head_ref=NULL;
+}
+
+int main()
+{
+    int i,j,n,e,pos;
+    struct node* head=NULL;
+    //head=(struct node* )malloc(sizeof(struct node));
+    printf("enter the number of elements you want to insert\n");
+    scanf("%d",&n);
+    printf("enter the elements:\n");
+    for(j=0;j<n;j++){
+        scanf("%d",&e);
+        insert_beg(&head,e);
+    }
+
+    printf("The linked list is:\n");
+    printlist(head);
+    printf("enter choice\n1--delete a particular key.\n2--delete a node at particular location.\n3--Delete the whole list\n");
+    scanf("%d",&j);
+    switch(j)
+    {
+        case 1:
+            printf("Enter the key to be deleted:\n");
+            scanf("%d",&i);
+            deletenode(&head,i);
+            printf("\n");
+            printlist(head);
+        break;
+       case 2:
+           printf("Enter the position:\n");
+            scanf("%d",&pos);
+            deletepos(&head,pos);         //indexing starts from 0
+            printf("\n");
+            printlist(head);
+            break;
+       case 3:
+            dellist(&head);
+            printf("The list is deleted\n");
+            printlist(head);
+        break;
+
+    }
+
+    return 0;
+
+}
---
+++ linkedlist.c
@@ -0,0 +1,105 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+struct node{
+    int data;
+    struct node* next;
+};
+
+ void printlist(struct node *n)
+ {
+        while(n!=NULL)
+            {
+            printf("%d\t",n->data);
+            n=n->next;
+            }
+    }
+void insert_beg(struct node** head_ref ,int val )
+{
+    struct node* beg=(struct node** )malloc(sizeof(struct node));   //node to be inserted in beginning
+    beg->data=val;
+    beg->next=*head_ref;
+    *head_ref=beg;
+}
+
+void insert_end(struct node** head_ref,int val )
+{
+    struct node* end=(struct node* )malloc(sizeof(struct node)); //node to be inserted at end
+     struct node* last=*head_ref;
+
+    end->data=val;
+    end->next=NULL;
+
+    if(*head_ref == NULL){
+        head_ref=end;
+        return;
+    }
+
+    while(last->next != NULL)
+    {
+         last=last->next;
+    }
+    last->next=end;
+    //end->next=NULL;
+    return;
+}
+
+void insert_mid(struct node* prev,int val)
+{
+    if(prev == NULL){
+        //insert_end(&head,val);
+        printf("PREVIOUS CAN NOT BE NULL\n");
+        return;
+    }
+    struct node* mid=(struct node* )malloc(sizeof(struct node));
+    mid->data=val;
+    mid->next=prev->next;
+    prev->next=mid;
+
+}
+
+int main()
+{   int v,i,w,pos;
+    struct node* head=NULL;
+    struct node* sec=NULL;
+    struct node* third=NULL;
+
+    head=(struct node* )malloc(sizeof(struct node));
+    sec=(struct node* )malloc(sizeof(struct node));
+    third=(struct node* )malloc(sizeof(struct node));
+
+    head->data=1;
+    head->next=sec;
+
+    sec->data=2;
+    sec->next=third;
+
+    third->data=3;
+    third->next=NULL;
+
+    printf("Enter\n 1--Insertion at beginning\n 2--Insertion after some node\n 3--Insertion at end\n");
+    scanf("%d",&i);
+    switch(i)
+    {
+        case 1:printf("Enter the value you want to add on:\n");
+               scanf("%d",&v);
+               insert_beg(&head,v);
+               break;
+        case 3:printf("Enter the value you want to insert at end:\n");
+               scanf("%d",&w);
+               insert_end(&head,w);
+               break;
+        case 2://printf("Enter the location where you want to insert\n");
+               //scanf("%d",&pos);
+               printf("Enter the value you want to insert after the second element\n");
+               scanf("%d",&v);
+               insert_mid(head->next,v);
+               break;
+    }
+
+    printlist(head);
+
+    return 0;
+
+}
